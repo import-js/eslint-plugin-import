28eed02e0a8924a920e7781ce7a20e5f390a249d
Ben Mosher <me@benmosher.com>
2015-03-19 07:12:02 -0400

    Rule added to index + bumped version to 0.3.0.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 exports.rules = {
   "exists": require("./lib/rules/exists"),
-  "named": require("./lib/rules/named")
+  "named": require("./lib/rules/named"),
+  "default": require("./lib/rules/default")
 };
 
 exports.rulesConfig = {
   "exists": 2,
-  "named": 2
+  "named": 2,
+  "default": 2
 };
