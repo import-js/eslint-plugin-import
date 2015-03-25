521b5309bd3f1321149c430d80a4436f685f8f91
Ben Mosher <me@benmosher.com>
2015-03-25 06:59:57 -0400

    Added namespace assignment report to `no-reassign`.

--- tests/lib/rules/no-reassign.js
+++ tests/lib/rules/no-reassign.js
@@ -9,18 +9,31 @@ var test = require("../../utils").test;
 
 eslintTester.addRuleTest("lib/rules/no-reassign", {
   valid: [
-    test({code: "import { foo } from './bar'; bar = 42;"})
+    test({code: "import { foo } from './bar'; bar = 42;"}),
+    test({code: "import { foo } from './bar'; function bar(foo) {};"}) // shadowing is legal
   ],
 
   invalid: [
+    // assignment to shadow is invalid
+    test({
+      code: "import { foo } from './bar'; function bar(foo) { foo = 42; };",
+      errors: [{ message: "Reassignment of local imported name 'foo'."}]}),
+
     test({
       code: "import { foo } from './bar'; foo = 42;",
       errors: [{ message: "Reassignment of local imported name 'foo'." }]}),
+
     test({
       code: "import foo from './bar'; foo = 42;",
       errors: [{ message: "Reassignment of local imported name 'foo'." }]}),
+
     test({
       code: "import * as foo from './bar'; foo = 42;",
-      errors: [{ message: "Reassignment of local imported name 'foo'." }]})
+      errors: [{ message: "Reassignment of local imported name 'foo'." }]}),
+
+    test({
+      code: "import * as foo from './bar'; foo.x = 'y';",
+      errors: [{ message: "Assignment to member of namespace 'foo'."}]
+    })
   ]
 });
