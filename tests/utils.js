3570266f2df8c460d7667b5f53445c88d11af972
Ben Mosher <me@benmosher.com>
2015-03-31 06:22:11 -0400

    Moved test files under tests directory.

--- tests/utils.js
+++ tests/utils.js
@@ -4,7 +4,7 @@ var
   assign = require("object-assign"),
   path = require("path");
 
-var FILENAME = path.join(process.cwd(), "./files", "foo.js");
+var FILENAME = path.join(process.cwd(), "./tests/files", "foo.js");
 
 exports.test = function test(t) {
   return assign({filename: FILENAME, ecmaFeatures: {modules: true}}, t);
