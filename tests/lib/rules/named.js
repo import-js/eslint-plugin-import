bfe33afe4add7777bbb8402d1d6dd03f6246c5e3
Ben Mosher <me@benmosher.com>
2015-06-05 08:15:44 -0400

    cleanup

--- tests/lib/rules/named.js
+++ tests/lib/rules/named.js
@@ -1,58 +1,63 @@
-"use strict";
+'use strict'
 
-var linter = require("eslint").linter,
-    ESLintTester = require("eslint-tester");
+var linter = require('eslint').linter,
+    ESLintTester = require('eslint-tester')
 
-var eslintTester = new ESLintTester(linter);
+var eslintTester = new ESLintTester(linter)
 
-var test = require("../../utils").test;
+var test = require('../../utils').test
 
 function error(name, module) {
-  return { message: name + " not found in '" + module + "'", type: "Identifier" };
+  return { message: name + ' not found in \'' + module + '\''
+         , type: 'Identifier' }
 }
 
 
-eslintTester.addRuleTest("lib/rules/named", {
+eslintTester.addRuleTest('lib/rules/named', {
   valid: [
-    test({code: "import { foo } from './bar';"}),
-    test({code: "import { foo } from './empty-module';"}),
-    test({code: "import bar from './bar.js';"}),
-    test({code: "import bar, { foo } from './bar.js';"}),
-    test({code: "import {a, b, d} from './named-exports';"}),
-    test({code: "import {ExportedClass} from './named-exports';"}),
-    test({code: "import { ActionTypes } from './qc';"}),
-    test({code: "import {a, b, c, d} from './re-export';"}),
-    test({code: "import {foo, bar} from './re-export-names';", args: [2, "es6-only"]})
+    test({code: 'import { foo } from "./bar"'}),
+    test({code: 'import { foo } from "./empty-module"'}),
+    test({code: 'import bar from "./bar.js"'}),
+    test({code: 'import bar, { foo } from "./bar.js"'}),
+    test({code: 'import {a, b, d} from "./named-exports"'}),
+    test({code: 'import {ExportedClass} from "./named-exports"'}),
+    test({code: 'import { ActionTypes } from "./qc"'}),
+    test({code: 'import {a, b, c, d} from "./re-export"'}),
+
+    test({ code: 'import {foo, bar} from "./re-export-names"'
+         , args: [2, 'es6-only']})
   ],
 
   invalid: [
-    test({code: "import {a, b, d} from './common';",
-      errors: [error("a", "./common"), error("b", "./common"), error("d", "./common")]}),
+    test({code: 'import {a, b, d} from "./common"',
+      errors: [ error('a', './common')
+              , error('b', './common')
+              , error('d', './common') ]}),
 
-    test({code: "import { baz } from './bar';",
-      errors: [error("baz", "./bar")]}),
+    test({code: 'import { baz } from "./bar"',
+      errors: [error('baz', './bar')]}),
 
     // test multiple
-    test({code: "import { baz, bop } from './bar';",
-      errors: [error("baz", "./bar"), error("bop", "./bar")]}),
+    test({code: 'import { baz, bop } from "./bar"',
+      errors: [error('baz', './bar'), error('bop', './bar')]}),
 
-    test({code: "import {a, b, c} from './named-exports';",
-      errors: [error("c", "./named-exports")]}),
+    test({code: 'import {a, b, c} from "./named-exports"',
+      errors: [error('c', './named-exports')]}),
 
-    test({code: "import { a } from './default-export';",
-      errors: [error("a", "./default-export")]}),
+    test({code: 'import { a } from "./default-export"',
+      errors: [error('a', './default-export')]}),
 
-    test({code: "import { a } from './common';", args: [2, "es6-only"],
-      errors: [error("a", "./common")]}),
+    test({code: 'import { a } from "./common"', args: [2, 'es6-only'],
+      errors: [error('a', './common')]}),
 
-    test({code: "import { ActionTypess } from './qc';",
-      errors: [error("ActionTypess", "./qc")]}),
+    test({code: 'import { ActionTypess } from "./qc"',
+      errors: [error('ActionTypess', './qc')]}),
 
-    test({code: "import {a, b, c, d, e} from './re-export';",
-      errors: [error("e", "./re-export")]}),
+    test({code: 'import {a, b, c, d, e} from "./re-export"',
+      errors: [error('e', './re-export')]}),
 
-    test({code: "import { a } from './re-export-names';",
-      args: [2, "es6-only"],
-        errors: [error("a", "./re-export-names")]})
+    test({code: 'import { a } from "./re-export-names"',
+      args: [2, 'es6-only'],
+        errors: [error('a', './re-export-names')]})
   ]
-});
+})
