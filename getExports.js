07cfda640fc8dc60f9945c94734f77ef39a88b8c
Ben Mosher <me@benmosher.com>
2015-06-08 13:07:53 -0400

    cleanup

--- getExports.js
+++ getExports.js
@@ -1,21 +1,21 @@
-"use strict";
+'use strict'
 
 var
-  Map = require("es6-map"),
-  Set = require("es6-set"),
-  parse = require("./lib/parse"),
-  resolve = require("./resolve"),
-  isCore = require("resolve").isCore;
+  Map = require('es6-map'), // eslint-disable-line no-native-reassign
+  Set = require('es6-set'), // eslint-disable-line no-native-reassign
+  parse = require('./lib/parse'),
+  resolve = require('./resolve'),
+  isCore = require('resolve').isCore
 
-var exportCache = new Map();
+var exportCache = new Map()
 
 function ExportMap(settings) {
   this.settings = settings
 
-  this.hasDefault = false;
-  this.named = new Set();
+  this.hasDefault = false
+  this.named = new Set()
 
-  this.errors = [];
+  this.errors = []
 }
 
 ExportMap.get = function (source, context) {
@@ -34,8 +34,8 @@ ExportMap.for = function (path, settings) {
 
   exportCache.set(path, exportMap)
 
-  // Object.freeze(exportMap);
-  // Object.freeze(exportMap.named);
+  // Object.freeze(exportMap)
+  // Object.freeze(exportMap.named)
 
   return exportMap
 }
@@ -43,32 +43,32 @@ ExportMap.for = function (path, settings) {
 ExportMap.parse = function (path, settings) {
   var m = new ExportMap(settings)
 
-  if (isCore(path)) return m; // skip parsing
+  if (isCore(path)) return m // skip parsing
 
   try {
-    var ast = parse(path);
+    var ast = parse(path)
   } catch (err) {
-    m.errors.push(err);
-    return m; // can't continue
+    m.errors.push(err)
+    return m // can't continue
   }
 
   ast.body.forEach(function (n) {
-    m.captureDefault(n);
-    m.captureAll(n, path);
-    m.captureNamedDeclaration(n);
-  });
+    m.captureDefault(n)
+    m.captureAll(n, path)
+    m.captureNamedDeclaration(n)
+  })
 
-  return m;
-};
+  return m
+}
 
 ExportMap.prototype.captureDefault = function (n) {
-  if (n.type !== "ExportDefaultDeclaration") return;
+  if (n.type !== 'ExportDefaultDeclaration') return
 
-  this.hasDefault = true;
-};
+  this.hasDefault = true
+}
 
 ExportMap.prototype.captureAll = function (n, path) {
-  if (n.type !== "ExportAllDeclaration") return
+  if (n.type !== 'ExportAllDeclaration') return
 
   var remotePath = resolve.relative(n.source.value, path)
   if (remotePath == null) return
@@ -79,26 +79,27 @@ ExportMap.prototype.captureAll = function (n, path) {
 }
 
 ExportMap.prototype.captureNamedDeclaration = function (n) {
-  if (n.type !== "ExportNamedDeclaration") return;
+  if (n.type !== 'ExportNamedDeclaration') return
 
   // capture declaration
-  if (n.declaration != null)
+  if (n.declaration != null) {
     switch (n.declaration.type) {
-      case "FunctionDeclaration":
-      case "ClassDeclaration":
-        this.named.add(n.declaration.id.name);
-        break;
-      case "VariableDeclaration":
+      case 'FunctionDeclaration':
+      case 'ClassDeclaration':
+        this.named.add(n.declaration.id.name)
+        break
+      case 'VariableDeclaration':
         n.declaration.declarations.forEach(function (d) {
-          this.named.add(d.id.name);
-        }.bind(this));
-        break;
+          this.named.add(d.id.name)
+        }.bind(this))
+        break
+    }
   }
 
   // capture specifiers
   n.specifiers.forEach(function (s) {
-    this.named.add(s.exported.name);
-  }.bind(this));
-};
+    this.named.add(s.exported.name)
+  }.bind(this))
+}
 
-module.exports = ExportMap.get;
+module.exports = ExportMap.get
