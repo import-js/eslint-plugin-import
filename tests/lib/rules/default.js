b150322de42331a6077b55e1173cb0edf055d7d6
Ben Mosher <me@benmosher.com>
2015-06-08 13:02:08 -0400

    cleanup

--- tests/lib/rules/default.js
+++ tests/lib/rules/default.js
@@ -1,36 +1,39 @@
-"use strict";
+'use strict'
 
-var linter = require("eslint").linter,
-    ESLintTester = require("eslint-tester");
+var linter = require('eslint').linter,
+    ESLintTester = require('eslint-tester')
 
-var eslintTester = new ESLintTester(linter);
+var eslintTester = new ESLintTester(linter)
 
-var test = require("../../utils").test;
+var test = require('../../utils').test
 
-eslintTester.addRuleTest("lib/rules/default", {
+eslintTester.addRuleTest('lib/rules/default', {
   valid: [
-    test({code: "import foo from './empty-folder';"}),
-    test({code: "import { foo } from './default-export';"}),
-    test({code: "import foo from './default-export';"}),
-    test({code: "import foo from './mixed-exports';"}),
+    test({code: 'import foo from "./empty-folder";'}),
+    test({code: 'import { foo } from "./default-export";'}),
+    test({code: 'import foo from "./default-export";'}),
+    test({code: 'import foo from "./mixed-exports";'}),
     test({
-      code: "import bar from './default-export';"}),
+      code: 'import bar from "./default-export";'}),
     test({
-      code: "import CoolClass from './default-class';"}),
+      code: 'import CoolClass from "./default-class";'}),
     test({
-      code: "import bar, { baz } from './default-export';"})
+      code: 'import bar, { baz } from "./default-export";'})
   ],
 
   invalid: [
     test({
-      code: "import crypto from 'crypto';",
-      errors: [{message: "No default export found in module.", type: "ImportDefaultSpecifier"}]}),
+      code: 'import crypto from "crypto";',
+      errors: [{ message: 'No default export found in module.'
+               , type: 'ImportDefaultSpecifier'}]}),
     test({
-      code: "import baz from './named-exports';",
-      errors: [{message: "No default export found in module.", type: "ImportDefaultSpecifier"}]}),
+      code: 'import baz from "./named-exports";',
+      errors: [{ message: 'No default export found in module.'
+               , type: 'ImportDefaultSpecifier'}]}),
 
     test({
-      code: "import bar from './common';",
-      errors: [{message: "No default export found in module.", type: "ImportDefaultSpecifier"}]})
+      code: 'import bar from "./common";',
+      errors: [{ message: 'No default export found in module.'
+               , type: 'ImportDefaultSpecifier'}]})
   ]
-});
+})
