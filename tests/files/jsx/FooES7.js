c3da130f5ab42794ddaf78ff1695057efb6ec5b8
Ben Mosher <me@benmosher.com>
2015-07-16 16:17:51 -0400

    explicit tests for ES7 parser errors being reported by `no-errors` (close #36)

---
+++ tests/files/jsx/FooES7.js
@@ -0,0 +1,9 @@
+// see issue #36
+
+// Foo.jsx
+class Foo {
+    // ES7 static members
+    static bar = true;
+}
+
+export default Foo;
--- tests/lib/rules/no-errors.js
+++ tests/lib/rules/no-errors.js
@@ -13,13 +13,22 @@ eslintTester.addRuleTest("lib/rules/no-errors", {
     test({code: "import { foo } from './empty-folder';"}),
     test({code: "import { foo } from './does-not-exist';"}),
     test({code: "import { a } from './test'"})
+
+  , test({ code: "import Foo from './jsx/FooES7.js';", args: [0] })
   ],
 
   invalid: [
+    test({ code: "import Foo from './jsx/FooES7.js';",
+      errors: [{
+        message: "Errors encountered while analysing imported module './jsx/FooES7.js'.",
+        type: "Literal"}]}),
+
+
     test({code: "import { a } from './test.coffee';",
       errors: [{
         message: "Errors encountered while analysing imported module './test.coffee'.",
         type: "Literal"}]}),
+
     test({code: "import foo from './malformed.js'",
       errors: [{
         message: "Errors encountered while analysing imported module './malformed.js'.",
