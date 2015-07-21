5b8d6cc7c0f71e87a603cf51bb4a769e32a08a1a
Ben Mosher <me@benmosher.com>
2015-07-21 05:46:25 -0400

    test cleanup

--- tests/src/rules/no-unresolved.js
+++ tests/src/rules/no-unresolved.js
@@ -1,54 +1,48 @@
-"use strict";
+var path = require('path')
 
-var path = require("path");
+var test = require('../../utils').test
 
-var test = require("../../utils").test;
+var linter = require('eslint').linter,
+    ESLintTester = require('eslint-tester')
 
-var linter = require("eslint").linter,
-    ESLintTester = require("eslint-tester");
+var eslintTester = new ESLintTester(linter)
 
-var eslintTester = new ESLintTester(linter);
-
-eslintTester.addRuleTest("lib/rules/no-unresolved", {
+eslintTester.addRuleTest('lib/rules/no-unresolved', {
   valid: [
-    test({ code: "import foo from './bar';" }),
-    test({ code: "import foo from './bar';"
-         , args: [2, 'relative-only']
-         }),
-    test({
-      code: "import bar from './bar.js';"}),
-    test({
-      code: "import {someThing} from './module';"}),
-    test({
-      code: "import fs from 'fs';"}),
+    test({ code: 'import foo from "./bar";' }),
+    test({ code: "import bar from './bar.js';" }),
+    test({ code: "import {someThing} from './module';" }),
+    test({ code: "import fs from 'fs';" }),
 
     test({
       code: "import { DEEP } from 'in-alternate-root';",
       settings: {
-        "import/resolve": {
-          "paths": [path.join(process.cwd(), "tests", "files", "alternate-root")]
+        'import/resolve': {
+          'paths': [path.join( process.cwd()
+                             , 'tests', 'files', 'alternate-root')]
         }
       }
     }),
     test({
-      code: "import { DEEP } from 'in-alternate-root'; import { bar } from 'src-bar';",
-      settings: {"import/resolve": { 'paths': [
-        path.join("tests", "files", "src-root"),
-        path.join("tests", "files", "alternate-root")
+      code: "import { DEEP } from 'in-alternate-root'; " +
+            "import { bar } from 'src-bar';",
+      settings: {'import/resolve': { 'paths': [
+        path.join('tests', 'files', 'src-root'),
+        path.join('tests', 'files', 'alternate-root')
       ]}}}),
 
     test({ code: 'import * as foo from "a"' }),
 
     test({ code: 'import * as foo from "jsx-module/foo"'
          , settings: { 'import/resolve': { 'extensions': ['.jsx'] } }
-         , errors: [ {message:'Unable to resolve path to module \'jsx-module/foo\'.'} ]
          })
     ],
 
   invalid: [
     // should fail for jsx by default
     test({ code: 'import * as foo from "jsx-module/foo"'
-         , errors: [ {message:'Unable to resolve path to module \'jsx-module/foo\'.'} ]
+         , errors: [ {message: 'Unable to resolve path to ' +
+                               'module \'jsx-module/foo\'.'} ]
          }),
 
 
@@ -60,19 +54,26 @@ eslintTester.addRuleTest("lib/rules/no-unresolved", {
 
     test({
       code: "import bar from './baz';",
-      errors: [{message: "Unable to resolve path to module './baz'.", type: "Literal"}]}),
+      errors: [{ message: "Unable to resolve path to module './baz'."
+               , type: 'Literal'
+               }]}),
     test({ code: "import bar from './baz';"
-         , errors: [{message: "Unable to resolve path to module './baz'.", type: "Literal"}]
-         , args: [2, 'relative-only']
+         , errors: [{ message: "Unable to resolve path to module './baz'."
+                    , type: 'Literal'
+                    }]
          }),
     test({
       code: "import bar from './empty-folder';",
-      errors: [{message: "Unable to resolve path to module './empty-folder'.", type: "Literal"}]}),
+      errors: [{ message: "Unable to resolve path to module './empty-folder'."
+               , type: 'Literal'
+               }]}),
 
     // sanity check that this module is _not_ found without proper settings
     test({
       code: "import { DEEP } from 'in-alternate-root';",
-      args: [2, 'all'],
-      errors: [{message: "Unable to resolve path to module 'in-alternate-root'.", type: "Literal"}]})
+      errors: [{ message: 'Unable to resolve path to ' +
+                          "module 'in-alternate-root'."
+               , type: 'Literal'
+               }]})
   ]
-});
+})
