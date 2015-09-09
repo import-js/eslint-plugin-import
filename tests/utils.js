87576fa37dd5768be2fcd9f1975d3a9a7379d6ac
Ben Mosher <me@benmosher.com>
2015-09-09 06:10:21 -0400

    Sped up babel-eslint test by requiring it outside a test. Closes #47

--- tests/utils.js
+++ tests/utils.js
@@ -1,5 +1,8 @@
 import path from 'path'
 
+// warms up the module cache. this import takes a while (>500ms)
+require('babel-eslint')
+
 export function testFilePath(relativePath) {
     return path.join(process.cwd(), './tests/files', relativePath)
 }
