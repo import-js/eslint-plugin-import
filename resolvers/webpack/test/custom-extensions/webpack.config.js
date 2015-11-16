382f8d4b07cd9f8964ae99d2acda0b666763158d
Ben Mosher <me@benmosher.com>
2015-11-16 08:29:37 -0500

    testing custom extensions

---
+++ resolvers/webpack/test/custom-extensions/webpack.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  resolve: { extensions: ['.js', '.coffee'] },
+}
--- resolvers/webpack/test/extensions.js
+++ resolvers/webpack/test/extensions.js
@@ -4,10 +4,27 @@ import resolve from '../index'
 import path from 'path'
 
 const file = path.join(__dirname, 'files', 'dummy.js')
+    , extensions = path.join(__dirname, 'custom-extensions', 'dummy.js')
 
 describe("extensions", () => {
   it("respects the defaults", () => {
-    expect(resolve('./extensions/foo', file)).to.exist
-        .and.equal(path.join(__dirname, 'files', 'extensions', 'foo.web.js'))
+    expect(resolve('./foo', file)).to.exist
+        .and.equal(path.join(__dirname, 'files', 'foo.web.js'))
+  })
+
+  describe("resolve.extensions set", () => {
+    it("works", () => {
+      expect(resolve('./foo', extensions)).to.exist
+        .and.equal(path.join(__dirname, 'custom-extensions', 'foo.js'))
+    })
+
+    it("replaces defaults", () => {
+      expect(() => resolve('./baz', extensions)).to.throw(Error)
+    })
+
+    it("finds .coffee", () => {
+      expect(resolve('./bar', extensions)).to.exist
+        .and.equal(path.join(__dirname, 'custom-extensions', 'bar.coffee'))
+    })
   })
 })
