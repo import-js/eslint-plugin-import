1434640b723feb2d181e4947eea350caeee61e68
Ben Mosher <me@benmosher.com>
2015-11-27 20:00:58 -0500

    extra test while tracking down a typo

--- resolvers/webpack/test/root.js
+++ resolvers/webpack/test/root.js
@@ -1,13 +1,19 @@
-import { expect } from 'chai'
+var chai =  require('chai')
+  , expect = chai.expect
+
 import { resolveImport as resolve } from '../index'
 
 import path from 'path'
 
-const file = path.join(__dirname, 'files', 'src', 'dummy.js')
+var file = path.join(__dirname, 'files', 'src', 'dummy.js')
 
-describe("root", () => {
-  it("works", () => {
+describe("root", function () {
+  it("works", function () {
     expect(resolve('main-module', file)).to.exist
       .and.equal(path.join(__dirname, 'files', 'src', 'main-module.js'))
+  })
+  it("really works", function () {
+    expect(resolve('jsx/some-file', file)).to.exist
+      .and.equal(path.join(__dirname, 'files', 'src', 'jsx', 'some-file.js'))
   })
 })
