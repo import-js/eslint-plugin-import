06a8f2bedcff6f88d52b4157de38a1f7f45ad944
Ben Mosher <me@benmosher.com>
2016-02-06 16:32:15 -0500

    using parserPath from context to require parser

--- src/core/parse.js
+++ src/core/parse.js
@@ -1,8 +1,15 @@
 import fs from 'fs'
 
-export default function (path, { parserOptions, parserName } = {}) {
+export default function (path, context) {
 
-  const parser = require(parserName)
+  if (context == null) throw new Error("need context to parse properly")
+
+  const { parserOptions, parserPath } = context
+
+  if (!parserPath) throw new Error("parserPath is required!")
+
+  // require the parser relative to the main module (i.e., ESLint)
+  const parser = require.main.require(parserPath)
 
   return parser.parse(
     fs.readFileSync(path, {encoding: 'utf8'}),
--- tests/src/core/parse.js
+++ tests/src/core/parse.js
@@ -5,22 +5,22 @@ import { getFilename } from '../utils'
 
 describe('parse(path, { settings, ecmaFeatures })', function () {
   it("doesn't support JSX by default", function () {
-    expect(() => parse(getFilename('jsx.js'))).to.throw(Error)
+    expect(() => parse(getFilename('jsx.js'), { parserPath: 'espree' })).to.throw(Error)
   })
-  it('infers jsx from ecmaFeatures when using stock Babylon', function () {
-    expect(() => parse(getFilename('jsx.js'), { ecmaFeatures: { jsx: true }}))
+  it('infers jsx from ecmaFeatures when using stock parser', function () {
+    expect(() => parse(getFilename('jsx.js'), { parserPath: 'espree', parserOptions: { sourceType: 'module', ecmaFeatures: { jsx: true } } }))
       .not.to.throw(Error)
   })
-  it('adds plugin if not found', function () {
-    expect(() => parse(getFilename('jsx.js'), {
-      ecmaFeatures: { jsx: true },
-      settings: { 'import/parse-options': { plugins: ['exportExtentions'] } },
-    })).not.to.throw(Error)
-  })
-  it('keeps plugin if found', function () {
-    expect(() => parse(getFilename('jsx.js'), {
-      ecmaFeatures: { jsx: true },
-      settings: { 'import/parse-options': { plugins: ['jsx'] } },
-    })).not.to.throw(Error)
-  })
+  // it('adds plugin if not found', function () {
+  //   expect(() => parse(getFilename('jsx.js'), {
+  //     ecmaFeatures: { jsx: true },
+  //     settings: { 'import/parse-options': { plugins: ['exportExtentions'] } },
+  //   })).not.to.throw(Error)
+  // })
+  // it('keeps plugin if found', function () {
+  //   expect(() => parse(getFilename('jsx.js'), {
+  //     ecmaFeatures: { jsx: true },
+  //     settings: { 'import/parse-options': { plugins: ['jsx'] } },
+  //   })).not.to.throw(Error)
+  // })
 })
