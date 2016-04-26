04ad742b01287a55b157af80cab9a1739117aca1
Radek Benkel <radoslaw.benkel@gmail.com>
2016-04-26 18:45:09 +0200

    Add support for `require`

--- tests/src/rules/newline-after-import.js
+++ tests/src/rules/newline-after-import.js
@@ -1,6 +1,7 @@
 import { RuleTester } from 'eslint'
 
-const ERROR_MESSAGE = 'Expected empty line after import statement not followed by another import.';
+const IMPORT_ERROR_MESSAGE = 'Expected empty line after import statement not followed by another import.';
+const REQUIRE_ERROR_MESSAGE = 'Expected empty line after require statement not followed by another require.';
 
 const ruleTester = new RuleTester()
 
@@ -11,6 +12,14 @@ ruleTester.run('newline-after-import', require('rules/newline-after-import'), {
       parserOptions: { sourceType: 'module' }
     },
     {
+      code: "var foo = require('foo-module');\n\nvar foo = 'bar';",
+      parserOptions: { sourceType: 'module' }
+    },
+    {
+      code: "require('foo-module');\n\nvar foo = 'bar';",
+      parserOptions: { sourceType: 'module' }
+    },
+    {
       code: "import foo from 'foo';\nimport { bar } from './bar-lib';",
       parserOptions: { sourceType: 'module' }
     },
@@ -18,6 +27,10 @@ ruleTester.run('newline-after-import', require('rules/newline-after-import'), {
       code: "import foo from 'foo';\n\nvar a = 123;\n\nimport { bar } from './bar-lib';",
       parserOptions: { sourceType: 'module' }
     },
+    {
+      code: "var foo = require('foo-module');\n\nvar a = 123;\n\nvar bar = require('bar-lib');",
+      parserOptions: { sourceType: 'module' }
+    }
   ],
 
   invalid: [
@@ -26,7 +39,16 @@ ruleTester.run('newline-after-import', require('rules/newline-after-import'), {
       errors: [ {
         line: 2,
         column: 1,
-        message: ERROR_MESSAGE
+        message: IMPORT_ERROR_MESSAGE
+      } ],
+      parserOptions: { sourceType: 'module' }
+    },
+    {
+      code: "var foo = require('foo-module');\nvar something = 123;",
+      errors: [ {
+        line: 2,
+        column: 1,
+        message: REQUIRE_ERROR_MESSAGE
       } ],
       parserOptions: { sourceType: 'module' }
     },
@@ -36,13 +58,43 @@ ruleTester.run('newline-after-import', require('rules/newline-after-import'), {
       {
         line: 2,
         column: 1,
-        message: ERROR_MESSAGE
+        message: IMPORT_ERROR_MESSAGE
       },
       {
         line: 5,
         column: 1,
-        message: ERROR_MESSAGE
+        message: IMPORT_ERROR_MESSAGE
       }],
+      parserOptions: { sourceType: 'module' }
+    },
+    {
+      code: "var foo = require('foo-module');\nvar a = 123;\n\nvar bar = require('bar-lib');\nvar b=456;",
+      errors: [
+        {
+          line: 2,
+          column: 1,
+          message: REQUIRE_ERROR_MESSAGE
+        },
+        {
+          line: 5,
+          column: 1,
+          message: REQUIRE_ERROR_MESSAGE
+        }],
+      parserOptions: { sourceType: 'module' }
+    },
+    {
+      code: "var foo = require('foo-module');\nvar a = 123;\n\nrequire('bar-lib');\nvar b=456;",
+      errors: [
+        {
+          line: 2,
+          column: 1,
+          message: REQUIRE_ERROR_MESSAGE
+        },
+        {
+          line: 5,
+          column: 1,
+          message: REQUIRE_ERROR_MESSAGE
+        }],
       parserOptions: { sourceType: 'module' }
     },
   ]
