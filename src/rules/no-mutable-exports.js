218ed01a4575a4cf81afa5e21bbc8c4e42597987
Joshua Peek <josh@joshpeek.com>
2016-04-27 21:35:38 -0700

    Add no-mutable-exports

---
+++ src/rules/no-mutable-exports.js
@@ -0,0 +1,10 @@
+module.exports = function (context) {
+	return {
+		'ExportNamedDeclaration': function (node) {
+      const kind = node.declaration.kind
+      if (kind === 'var' || kind === 'let') {
+        context.report(node, `Exporting mutable '${kind}' binding, use const instead.`)
+      }
+		},
+	}
+}
