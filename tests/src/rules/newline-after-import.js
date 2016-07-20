c23e7639f4b3e9004d11cdc6e3c189234efc0f6e
Ben Mosher <me@benmosher.com>
2016-07-20 06:35:36 -0400

    Fix #441 + added a bunch of debug logging for next time 😅

--- tests/src/rules/newline-after-import.js
+++ tests/src/rules/newline-after-import.js
@@ -32,6 +32,39 @@ ruleTester.run('newline-after-import', require('rules/newline-after-import'), {
           return renderData.mainModalContent.clone()
       }
     }`,
+    { code: `//issue 441
+    function bar() {
+      switch (foo) {
+        case '1':
+          return require('../path/to/file1.jst.hbs')(renderData, options);
+        case '2':
+          return require('../path/to/file2.jst.hbs')(renderData, options);
+        case '3':
+          return require('../path/to/file3.jst.hbs')(renderData, options);
+        case '4':
+          return require('../path/to/file4.jst.hbs')(renderData, options);
+        case '5':
+          return require('../path/to/file5.jst.hbs')(renderData, options);
+        case '6':
+          return require('../path/to/file6.jst.hbs')(renderData, options);
+        case '7':
+          return require('../path/to/file7.jst.hbs')(renderData, options);
+        case '8':
+          return require('../path/to/file8.jst.hbs')(renderData, options);
+        case '9':
+          return require('../path/to/file9.jst.hbs')(renderData, options);
+        case '10':
+          return require('../path/to/file10.jst.hbs')(renderData, options);
+        case '11':
+          return require('../path/to/file11.jst.hbs')(renderData, options);
+        case '12':
+          return something();
+        default:
+          return somethingElse();
+      }
+    }`,
+      parserOptions: { sourceType: 'module' },
+    },
     {
       code: "import path from 'path';\nimport foo from 'foo';\n",
       parserOptions: { sourceType: 'module' },
@@ -196,5 +229,5 @@ ruleTester.run('newline-after-import', require('rules/newline-after-import'), {
       } ],
       parserOptions: { sourceType: 'module' },
     },
-  ]
+  ],
 })
