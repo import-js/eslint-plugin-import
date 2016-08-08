3babc1b5be00181a4690b705fedb687867ac61fa
Jimmy Jia <tesrin@gmail.com>
2016-08-08 15:11:53 -0400

    Add support for 'module' entry point

--- resolvers/webpack/test/packageMains.js
+++ resolvers/webpack/test/packageMains.js
@@ -9,6 +9,11 @@ var file = path.join(__dirname, 'package-mains', 'dummy.js')
 
 describe("packageMains", function () {
 
+  it("captures module", function () {
+    expect(webpack.resolve('./module', file)).property('path')
+      .to.equal(path.join(__dirname, 'package-mains', 'module', 'src', 'index.js'))
+  })
+
   it("captures jsnext", function () {
     expect(webpack.resolve('./jsnext', file)).property('path')
       .to.equal(path.join(__dirname, 'package-mains', 'jsnext', 'src', 'index.js'))
@@ -27,6 +32,11 @@ describe("packageMains", function () {
   it("uses configured packageMains, if provided", function () {
     expect(webpack.resolve('./webpack', file, { config: 'webpack.alt.config.js' })).property('path')
       .to.equal(path.join(__dirname, 'package-mains', 'webpack', 'index.js'))
+  })
+
+  it("always defers to module, regardless of config", function () {
+    expect(webpack.resolve('./module', file, { config: 'webpack.alt.config.js' })).property('path')
+      .to.equal(path.join(__dirname, 'package-mains', 'module', 'src', 'index.js'))
   })
 
   it("always defers to jsnext:main, regardless of config", function () {
