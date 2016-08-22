435e31243fc27cb0d87efc17f581b940b5e23e91
Jeroen Engels <jfm.engels@gmail.com>
2016-08-22 21:26:14 +0200

    Fix not working in Node v4

--- src/rules/no-nodejs-modules.js
+++ src/rules/no-nodejs-modules.js
@@ -2,7 +2,7 @@ import importType from '../core/importType'
 import isStaticRequire from '../core/staticRequire'
 
 function reportIfMissing(context, node, allowed, name) {
-  if (!allowed.includes(name) && importType(name, context) === 'builtin') {
+  if (allowed.indexOf(name) === -1 && importType(name, context) === 'builtin') {
     context.report(node, 'Do not import Node.js builtin module "' + name + '"')
   }
 }
