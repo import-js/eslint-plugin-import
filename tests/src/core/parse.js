d0007f269c82e648e724dde509245082eaed10f5
Ivan Babak <babak.john@gmail.com>
2017-05-26 14:33:41 -0700

    Add sinon, replace eslint-module-utils test spy with sinon.spy

--- tests/src/core/parse.js
+++ tests/src/core/parse.js
@@ -1,8 +1,9 @@
 import * as fs from 'fs'
 import { expect } from 'chai'
+import sinon from 'sinon'
 import parse from 'eslint-module-utils/parse'
 
-import { getFilename, makeNaiveSpy } from '../utils'
+import { getFilename } from '../utils'
 
 describe('parse(content, { settings, ecmaFeatures })', function () {
   const path = getFilename('jsx.js')
@@ -22,20 +23,20 @@ describe('parse(content, { settings, ecmaFeatures })', function () {
   })
 
   it('passes expected parserOptions to custom parser', function () {
-    const parseSpy = makeNaiveSpy()
+    const parseSpy = sinon.spy()
     const parserOptions = { ecmaFeatures: { jsx: true } }
     require('./parseStubParser').parse = parseSpy
     parse(path, content, { settings: {}, parserPath: require.resolve('./parseStubParser'), parserOptions: parserOptions })
-    expect(parseSpy.callCount).to.equal(1)
-    expect(parseSpy.lastCallArguments[0]).to.equal(content)
-    expect(parseSpy.lastCallArguments[1]).to.be.an('object')
-    expect(parseSpy.lastCallArguments[1]).to.not.equal(parserOptions)
-    expect(parseSpy.lastCallArguments[1])
+    expect(parseSpy.callCount, 'parse to be called once').to.equal(1)
+    expect(parseSpy.args[0][0], 'parse to get content as its first argument').to.equal(content)
+    expect(parseSpy.args[0][1], 'parse to get an object as its second argument').to.be.an('object')
+    expect(parseSpy.args[0][1], 'parse to clone the parserOptions object').to.not.equal(parserOptions)
+    expect(parseSpy.args[0][1], 'parse to get ecmaFeatures in parserOptions which is a clone of ecmaFeatures passed in')
       .to.have.property('ecmaFeatures')
         .that.is.eql(parserOptions.ecmaFeatures)
         .and.is.not.equal(parserOptions.ecmaFeatures)
-    expect(parseSpy.lastCallArguments[1]).to.have.property('attachComment', true)
-    expect(parseSpy.lastCallArguments[1]).to.have.property('filePath', path)
+    expect(parseSpy.args[0][1], 'parse to get parserOptions.attachComment equal to true').to.have.property('attachComment', true)
+    expect(parseSpy.args[0][1], 'parse to get parserOptions.filePath equal to the full path of the source file').to.have.property('filePath', path)
   })
 
 })
