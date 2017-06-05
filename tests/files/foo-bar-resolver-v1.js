117717f983094348fec9ad74b84685dbaaa50945
Ivan Babak <babak.john@gmail.com>
2017-06-05 10:30:01 -0700

    eslint-module-utils: Fix resolver tests for Windows paths
    https://ci.appveyor.com/project/benmosher/eslint-plugin-import/build/2055/job/wsl6hrhpc2dt10cs

--- tests/files/foo-bar-resolver-v1.js
+++ tests/files/foo-bar-resolver-v1.js
@@ -1,12 +1,11 @@
 var path = require('path')
 
 exports.resolveImport = function (modulePath, sourceFile, config) {
-  var fooPathSuffix = '/files/foo.js'
-  var exceptionPathSuffix = '/files/exception.js'
-  if (sourceFile.indexOf(fooPathSuffix) === sourceFile.length - fooPathSuffix.length) {
+  var sourceFileName = path.basename(sourceFile)
+  if (sourceFileName === 'foo.js') {
     return path.join(__dirname, 'bar.jsx')
   }
-  else if (sourceFile.indexOf(exceptionPathSuffix) === sourceFile.length - exceptionPathSuffix.length) {
+  else if (sourceFileName === 'exception.js') {
     throw new Error('foo-bar-resolver-v1 resolveImport test exception')
   }
   else {
--- tests/files/foo-bar-resolver-v2.js
+++ tests/files/foo-bar-resolver-v2.js
@@ -1,12 +1,11 @@
 var path = require('path')
 
 exports.resolve = function (modulePath, sourceFile, config) {
-  var fooPathSuffix = '/files/foo.js'
-  var exceptionPathSuffix = '/files/exception.js'
-  if (sourceFile.indexOf(fooPathSuffix) === sourceFile.length - fooPathSuffix.length) {
+  var sourceFileName = path.basename(sourceFile)
+  if (sourceFileName === 'foo.js') {
     return { found: true, path: path.join(__dirname, 'bar.jsx') }
   }
-  else if (sourceFile.indexOf(exceptionPathSuffix) === sourceFile.length - exceptionPathSuffix.length) {
+  else if (sourceFileName === 'exception.js') {
     throw new Error('foo-bar-resolver-v2 resolve test exception')
   }
   else {
