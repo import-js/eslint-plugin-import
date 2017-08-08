820e01db09de64f1b633a03d7ac453c31976dddb
Thomas Grainger <tom.grainger@procensus.com>
2017-08-08 10:47:11 +0100

    add no-useless-path-segments rule Fixes #471

---
+++ tests/src/rules/no-useless-path-segments.js
@@ -0,0 +1,27 @@
+import { test } from '../utils'
+import { RuleTester } from 'eslint'
+
+const ruleTester = new RuleTester()
+const rule = require('rules/no-useless-path-segments')
+
+function runResolverTests(resolver) {
+  ruleTester.run(`no-useless-path-segments (${resolver})`, rule, {
+    valid: [
+      test({ code: 'import "./malformed.js"' }),
+      test({ code: 'import fs from "fs"' }),
+    ],
+
+    invalid: [
+      test({
+        code: 'import "./../files/malformed.js"',
+        errors: [ 'Useless path segments for "./../files/malformed.js", should be "./malformed.js"'],
+      }),
+      test({
+        code: 'import "./../files/malformed"',
+        errors: [ 'Useless path segments for "./../files/malformed", should be "./malformed"'],
+      }),
+     ],
+   })
+}
+
+['node', 'webpack'].forEach(runResolverTests)
