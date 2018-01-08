60335bb3b1ebdc2a0d8444f36d41ad58e1ded7df
Landon Abney <Landonabney@gmail.com>
2018-01-08 14:18:43 -0800

    Support optional commit hash argument to docsUrl and add tests
    Add an optional second argument to `docsUrl` specifying a commit hash
    that, when specified, changes the returned URL to be specific to that
    hash instead of directing to `master`.
    
    Adds tests for `docsUrl` to ensure that it is operating as expected.

--- src/docsUrl.js
+++ src/docsUrl.js
@@ -1,5 +1,10 @@
-const ruleDocsUrl = 'https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules'
+const repoUrl = 'https://github.com/benmosher/eslint-plugin-import'
 
-export default function docsUrl(ruleName) {
-  return `${ruleDocsUrl}/${ruleName}.md`
+export default function docsUrl(ruleName, commitHash) {
+  let baseUrl = `${repoUrl}/tree/master/docs/rules`
+  if (commitHash) {
+    baseUrl = `${repoUrl}/blob/${commitHash}/docs/rules`
+  }
+
+  return `${baseUrl}/${ruleName}.md`
 }
---
+++ tests/src/core/docsUrl.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai'
+
+import docsUrl from '../../../src/docsUrl'
+
+describe('docsUrl', function () {
+  it('returns the rule documentation URL when given a rule name', function () {
+    expect(docsUrl('foo')).to.equal('https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules/foo.md')
+  })
+
+  it('supports an optional commit hash parameter', function () {
+    expect(docsUrl('foo', 'bar')).to.equal('https://github.com/benmosher/eslint-plugin-import/blob/bar/docs/rules/foo.md')
+  })
+})
