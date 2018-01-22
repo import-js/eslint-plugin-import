fa24082ab27e5689bbef2b5c885ccb55ffa93241
Suhas Karanth <sudo.suhas@gmail.com>
2018-01-22 09:14:38 +0530

    Use link to tagged version for rule docs

--- src/docsUrl.js
+++ src/docsUrl.js
@@ -1,5 +1,7 @@
+import pkg from '../package.json'
+
 const repoUrl = 'https://github.com/benmosher/eslint-plugin-import'
 
-export default function docsUrl(ruleName, commitHash = 'master') {
-  return `${repoUrl}/blob/${commitHash}/docs/rules/${ruleName}.md`
+export default function docsUrl(ruleName, commitish = `v${pkg.version}`) {
+  return `${repoUrl}/blob/${commitish}/docs/rules/${ruleName}.md`
 }
