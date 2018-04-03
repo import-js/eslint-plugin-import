16f678b6786e15b55a6e5985b112b785aba20898
Tim Fischbach <tfischbach@codevise.de>
2018-04-03 12:28:12 +0200

    Do not report type imports from flow-typed packages
    When importing types from packages that are defined in a file inside
    the `flow-typed` directory, prevent the `named` rule from reporting
    that the package itself does not provide an export with that name.
    
    Currently there is support for type imports from local files. Restrict
    these checks to imports with relative paths.

--- tests/files/with-flow-typed/flow-typed/npm/myflowtyped_v1.x.x.js
+++ tests/files/with-flow-typed/flow-typed/npm/myflowtyped_v1.x.x.js
@@ -1,4 +1,6 @@
 // flow-typed signature: ____
 // flow-typed version: ____/myflowtyped_v1.x.x/flow_>=v0.33.x
 
-declare module 'myflowtyped' {}
+declare module 'myflowtyped' {
+  declare export type MyType = boolean;
+}
