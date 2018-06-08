7312cf4ffcb65c6288d94d7364c8b5dddf6165f3
mieszko4 <mieszko4@gmail.com>
2018-06-08 12:32:59 +0200

    Use resolved package name
    Should fix https://github.com/benmosher/eslint-plugin-import/issues/496

--- src/rules/no-extraneous-dependencies.js
+++ src/rules/no-extraneous-dependencies.js
@@ -111,7 +111,8 @@ function reportIfMissing(context, deps, depsOptions, node, name) {
   const resolved = resolve(name, context)
   if (!resolved) { return }
 
-  const splitName = name.split('/')
+  const [, resolvedName = name] = resolved.split(process.cwd() + "/" + "node_modules/");
+  const splitName = resolvedName.split('/')
   const packageName = splitName[0][0] === '@'
     ? splitName.slice(0, 2).join('/')
     : splitName[0]
