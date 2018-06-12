e25180ce3681da00953bdf2dd1ccdb9c888e5e40
mieszko4 <mieszko4@gmail.com>
2018-06-12 16:14:29 +0200

    Fix extracting package name from resolved
    Assume folder with packages is node_modules and take part of path after last node_modules

--- src/rules/no-extraneous-dependencies.js
+++ src/rules/no-extraneous-dependencies.js
@@ -111,7 +111,9 @@ function reportIfMissing(context, deps, depsOptions, node, name) {
   const resolved = resolve(name, context)
   if (!resolved) { return }
 
-  const [, resolvedName = name] = resolved.split(path.join(process.cwd(), 'node_modules/'))
+  // Take last part after node_modules
+  const [resolvedName] = resolved.split('node_modules/').slice(-1)
+
   const splitName = resolvedName.split('/')
   const packageName = splitName[0][0] === '@'
     ? splitName.slice(0, 2).join('/')
