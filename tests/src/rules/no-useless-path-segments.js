2c1886e0094159beefc7c6571db83872590b1590
Pirasis <1pete@users.noreply.github.com>
2018-06-29 21:13:58 +0700

    make rule `no-useless-path-segments` work with commonjs

--- tests/src/rules/no-useless-path-segments.js
+++ tests/src/rules/no-useless-path-segments.js
@@ -7,6 +7,10 @@ const rule = require('rules/no-useless-path-segments')
 function runResolverTests(resolver) {
   ruleTester.run(`no-useless-path-segments (${resolver})`, rule, {
     valid: [
+      // commonjs with default options
+      test({ code: 'require("./../files/malformed.js")' }),
+
+      // esmodule
       test({ code: 'import "./malformed.js"' }),
       test({ code: 'import "./test-module"' }),
       test({ code: 'import "./bar/"' }),
@@ -16,6 +20,49 @@ function runResolverTests(resolver) {
     ],
 
     invalid: [
+      // commonjs
+      test({
+        code: 'require("./../files/malformed.js")',
+        options: [{ commonjs: true }],
+        errors: [ 'Useless path segments for "./../files/malformed.js", should be "../files/malformed.js"'],
+      }),
+      test({
+        code: 'require("./../files/malformed")',
+        options: [{ commonjs: true }],
+        errors: [ 'Useless path segments for "./../files/malformed", should be "../files/malformed"'],
+      }),
+      test({
+        code: 'require("../files/malformed.js")',
+        options: [{ commonjs: true }],
+        errors: [ 'Useless path segments for "../files/malformed.js", should be "./malformed.js"'],
+      }),
+      test({
+        code: 'require("../files/malformed")',
+        options: [{ commonjs: true }],
+        errors: [ 'Useless path segments for "../files/malformed", should be "./malformed"'],
+      }),
+      test({
+        code: 'require("./test-module/")',
+        options: [{ commonjs: true }],
+        errors: [ 'Useless path segments for "./test-module/", should be "./test-module"'],
+      }),
+      test({
+        code: 'require("./")',
+        options: [{ commonjs: true }],
+        errors: [ 'Useless path segments for "./", should be "."'],
+      }),
+      test({
+        code: 'require("../")',
+        options: [{ commonjs: true }],
+        errors: [ 'Useless path segments for "../", should be ".."'],
+      }),
+      test({
+        code: 'require("./deep//a")',
+        options: [{ commonjs: true }],
+        errors: [ 'Useless path segments for "./deep//a", should be "./deep/a"'],
+      }),
+
+      // esmodule
       test({
         code: 'import "./../files/malformed.js"',
         errors: [ 'Useless path segments for "./../files/malformed.js", should be "../files/malformed.js"'],
