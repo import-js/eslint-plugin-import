5ade15602cc5532d282f3eaead5d21407e455c47
e020873 <nicolas.henry-partner@arcelormittal.com>
2020-09-21 12:45:41 +0000

    [Fix] `no-unused-modules`: ignore flow type imports
    Fixes #1564.

--- tests/files/no-unused-modules/typescript/file-ts-a.ts
+++ tests/files/no-unused-modules/typescript/file-ts-a.ts
@@ -3,6 +3,6 @@ import {c} from './file-ts-c';
 import {d} from './file-ts-d';
 import {e} from './file-ts-e';
 
-export const a = b + 1 + e.f;
-export const a2: c = {};
-export const a3: d = {};
+const a = b + 1 + e.f;
+const a2: c = {};
+const a3: d = {};
