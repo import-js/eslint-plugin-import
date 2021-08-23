4aff27c6a25099f2617d0de7a6ddeb6a3fed3ff6
Jordan Harband <ljharb@gmail.com>
2021-08-23 16:59:22 -0700

    [Tests] test on TS parser v2, v3, and v4

--- tests/dep-time-travel.sh
+++ tests/dep-time-travel.sh
@@ -8,10 +8,16 @@ export NPM_CONFIG_LEGACY_PEER_DEPS=true
 
 npm install --no-save "eslint@${ESLINT_VERSION}" --ignore-scripts
 
-# completely remove the new TypeScript parser for ESLint < v5
-if [[ "$ESLINT_VERSION" -lt "5" ]]; then
+
+
+if [[ -n "$TS_PARSER" ]]; then # if TS parser is manually set, always use it
+  echo "Downgrading @typescript-eslint/parser..."
+  npm i --no-save "@typescript-eslint/parser@${TS_PARSER}"
+elif [[ "$ESLINT_VERSION" -lt "5" ]]; then # completely remove the new TypeScript parser for ESLint < v5
   echo "Removing @typescript-eslint/parser..."
   npm uninstall --no-save @typescript-eslint/parser
+elif [[ "$TRAVIS_NODE_VERSION" -lt "10" ]]; then # TS parser 3 requires node 10+
+  npm i --no-save "@typescript-eslint/parser@3"
 fi
 
 # use these alternate TypeScript dependencies for ESLint < v4
@@ -27,9 +33,4 @@ fi
 if [[ "$TRAVIS_NODE_VERSION" -lt "8" ]]; then
   echo "Downgrading eslint-import-resolver-typescript..."
   npm i --no-save eslint-import-resolver-typescript@1.0.2
-fi
-
-if [[ -n "$TS_PARSER" ]]; then
-  echo "Downgrading @typescript-eslint/parser..."
-  npm i --no-save @typescript-eslint/parser@2
 fi
