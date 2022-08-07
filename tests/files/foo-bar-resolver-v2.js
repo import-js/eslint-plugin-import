d0771ad61c40b0e14982f500133238d55e1a6b07
JounQin <admin@1stg.me>
2022-08-07 12:29:33 +0800

    [utils] [new] `resolve`: add `context` into resolver arguments
    Closes #2108

--- tests/files/foo-bar-resolver-v2.js
+++ tests/files/foo-bar-resolver-v2.js
@@ -1,6 +1,7 @@
+var assert = require('assert')
 var path = require('path')
 
-exports.resolve = function (modulePath, sourceFile, config) {
+exports.resolve = function (modulePath, sourceFile, config, options) {
   var sourceFileName = path.basename(sourceFile)
   if (sourceFileName === 'foo.js') {
     return { found: true, path: path.join(__dirname, 'bar.jsx') }
@@ -8,6 +9,9 @@ exports.resolve = function (modulePath, sourceFile, config) {
   if (sourceFileName === 'exception.js') {
     throw new Error('foo-bar-resolver-v2 resolve test exception')
   }
+
+  assert.ok(options.context, 'the `context` must be presented')
+
   return { found: false };
 };
 
