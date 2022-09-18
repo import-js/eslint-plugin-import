f344c3259ea1b3b02f81aea439f596ed2da8418f
Ron Park <ronpark.dev@gmail.com>
2022-09-18 22:14:29 +0900

    [New] `named-order` : add tester for named import

--- tests/src/rules/named-order.js
+++ tests/src/rules/named-order.js
@@ -7,7 +7,91 @@ const rule = require('rules/named-order');
 
 ruleTester.run('named-order', rule, {
   valid: [
+    //
+    // named import
+    //
+    test({
+      code: `import foo from 'foo'`,
+    }),
+    test({
+      code: `import {} from 'foo'`,
+    }),
+    test({
+      code: `import {a, b} from 'foo'`,
+    }),
+    test({
+      code: `import type {a, b} from 'foo'`,
+      parser: parsers.BABEL_OLD,
+    }),
+    test({
+      code: `import typeof {a, b} from 'foo'`,
+      parser: parsers.BABEL_OLD,
+    }),
+    test({
+      code: `import {type a, type b} from 'foo'`,
+      parser: parsers.BABEL_OLD,
+    }),
+    test({
+      code: `import {typeof a, typeof b} from 'foo'`,
+      parser: parsers.BABEL_OLD,
+    }),
+    test({
+      code: `import {typeof a, type b} from 'foo'`,
+      parser: parsers.BABEL_OLD,
+    }),
+    test({
+      code: `import {b, a} from 'foo'`,
+      options: [{ order: 'desc' }],
+    }),
+    test({
+      code: `import {A, a} from 'foo'`,
+      options: [{ caseInsensitive: true }],
+    }),
   ],
   invalid: [
+    //
+    // named import
+    //
+    test({
+      code: `import {b, a} from 'foo'`,
+      output: `import {a, b} from 'foo'`,
+      errors: ['Named import specifiers of `{b, a}` should sort as `{a, b}`'],
+    }),
+    test({
+      code: `import type {b, a} from 'foo'`,
+      output: `import type {a, b} from 'foo'`,
+      errors: ['Named import specifiers of `{b, a}` should sort as `{a, b}`'],
+      parser: parsers.BABEL_OLD,
+    }),
+    test({
+      code: `import typeof {b, a} from 'foo'`,
+      output: `import typeof {a, b} from 'foo'`,
+      errors: ['Named import specifiers of `{b, a}` should sort as `{a, b}`'],
+      parser: parsers.BABEL_OLD,
+    }),
+    test({
+      code: `import {type b, type a} from 'foo'`,
+      output: `import {type a, type b} from 'foo'`,
+      errors: ['Named import specifiers of `{type b, type a}` should sort as `{type a, type b}`'],
+      parser: parsers.BABEL_OLD,
+    }),
+    test({
+      code: `import {typeof b, typeof a} from 'foo'`,
+      output: `import {typeof a, typeof b} from 'foo'`,
+      errors: ['Named import specifiers of `{typeof b, typeof a}` should sort as `{typeof a, typeof b}`'],
+      parser: parsers.BABEL_OLD,
+    }),
+    test({
+      code: `import {typeof b, type a} from 'foo'`,
+      output: `import {type a, typeof b} from 'foo'`,
+      errors: ['Named import specifiers of `{typeof b, type a}` should sort as `{type a, typeof b}`'],
+      parser: parsers.BABEL_OLD,
+    }),
+    test({
+      code: `import {a, b} from 'foo'`,
+      output: `import {b, a} from 'foo'`,
+      errors: ['Named import specifiers of `{a, b}` should sort as `{b, a}`'],
+      options: [{ order: 'desc' }],
+    }),
   ],
 });
