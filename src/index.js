1ed4e7f7a1f8b52936cf5066fa1ee71b74ad67eb
Ron Park <ronpark.dev@gmail.com>
2022-09-18 22:15:04 +0900

    [New] `named-order` : add rule to root

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ export const rules = {
   'no-useless-path-segments': require('./rules/no-useless-path-segments'),
   'dynamic-import-chunkname': require('./rules/dynamic-import-chunkname'),
   'no-import-module-exports': require('./rules/no-import-module-exports'),
+  'named-order': require('./rules/named-order'),
 
   // export
   'exports-last': require('./rules/exports-last'),
