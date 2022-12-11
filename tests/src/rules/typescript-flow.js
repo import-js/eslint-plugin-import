bf6063a10a376f1f16a5640578090de9c165e368
Aziz Abdullaev <aziz.saidmuratov@duke.edu>
2022-12-10 21:31:28 -0500

    add valid cases for strict separate

--- tests/src/rules/typescript-flow.js
+++ tests/src/rules/typescript-flow.js
@@ -23,12 +23,31 @@ context('TypeScript', function () {
 
     ruleTester.run('typescript-flow', rule, { 
       valid: [
-      //   test({
-      //     code: `import type { MyType } from "./typescript.ts"`,
-      //     parser,
-      //     settings,
-      //     options: ['separate'],
-      //   }),
+        test({
+          code: `import type { MyType } from "./typescript.ts"`,
+          parser,
+          settings,
+          options: ['separate'],
+        }),
+        test({
+          code: `import * as Bar from "./typescript.ts"`,
+          parser,
+          settings,
+          options: ['separate'],
+        }),
+        // default import is ignored for now
+        test({
+          code: `import Bar from "./typescript.ts"`,
+          parser,
+          settings,
+          options: ['separate'],
+        }),
+        test({
+          code: `import type Bar from "./typescript.ts"`,
+          parser,
+          settings,
+          options: ['separate'],
+        }),
         // test({
         //   code: `import { MyType } from "./typescript.ts"`,
         //   parser,
@@ -83,7 +102,6 @@ context('TypeScript', function () {
         //   output: 'import { MyType, Bar } from "./typescript.ts"',
         // }),
 
-        // Single Config option which is separate
         test({
           code: 'import {type MyType,Bar} from "./typescript.ts"',
           parser,
@@ -116,7 +134,26 @@ context('TypeScript', function () {
             message: 'BOOM',
           }],
           output: 'import {Bar as Namespace} from "./typescript.ts"\nimport type { MyType } from "./typescript.ts"',
-          //output: 'import type { Bar } from "./typescript.ts"\nimport type { MyType } from "./typescript.ts"',
+        }),
+        test({
+          code: 'import {type MyType,type Foo} from "./typescript.ts"',
+          parser,
+          settings,
+          options: ['separate'],
+          errors: [{
+            message: 'BOOM',
+          }],
+          output: 'import type { MyType, Foo} from "./typescript.ts"',
+        }),
+        test({
+          code: 'import {type MyType as Bar,type Foo} from "./typescript.ts"',
+          parser,
+          settings,
+          options: ['separate'],
+          errors: [{
+            message: 'BOOM',
+          }],
+          output: 'import type { MyType as Bar, Foo} from "./typescript.ts"',
         }),
       ] },
     );
