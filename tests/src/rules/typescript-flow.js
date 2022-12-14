150af6f29ea3d73532c3e0883e73238b403c71d3
Aziz Abdullaev <aziz.saidmuratov@duke.edu>
2022-12-13 23:29:44 -0500

    add invalid test case with repeated import with namespace to inline option

--- tests/src/rules/typescript-flow.js
+++ tests/src/rules/typescript-flow.js
@@ -228,16 +228,26 @@ context('TypeScript', function () {
           }],
           output: 'import defaultExport, { type MyType as Persona, type Bar as Foo } from "./typescript.ts"',
         }),
-        // // TODO: what to do here? Output: import * as b, { type MyType, type Bar } from "./typescript.ts" ?
+        test({
+          code: 'import type {MyType, Bar} from "./typescript.ts";import * as b from "./typescript.ts"',
+          parser,
+          settings,
+          options: ['inline'],
+          errors: [{
+            message: 'BOOM',
+          }],
+          output: 'import  {type MyType, type Bar} from "./typescript.ts";import * as b from "./typescript.ts"',
+        }),
+        // TODO: Fix the one below
         // test({
-        //   code: 'import type {MyType, Bar} from "./typescript.ts";import * as b from "./typescript.ts"',
+        //   code: 'import type {MyType, Bar} from "./typescript.ts";import type A from "./typescript.ts"',
         //   parser,
         //   settings,
         //   options: ['inline'],
         //   errors: [{
         //     message: 'BOOM',
         //   }],
-        //   output: 'import defaultExport, { type MyType, type Bar } from "./typescript.ts"',
+        //   output: 'import  {type MyType, type Bar} from "./typescript.ts";import type A from "./typescript.ts"',
         // }),
       ] },
     );
