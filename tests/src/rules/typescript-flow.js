250a0227e0346ee383984008795b126148ff3a93
Aziz Abdullaev <aziz.saidmuratov@duke.edu>
2022-12-13 21:59:16 -0500

    add more named import test cases for strict and inline options

--- tests/src/rules/typescript-flow.js
+++ tests/src/rules/typescript-flow.js
@@ -48,7 +48,7 @@ context('TypeScript', function () {
           settings,
           options: ['separate'],
         }),
-        // default imports are ignored for strict option separate
+        // default imports are ignored for strict option separate. Question. TODO
         test({
           code: `import Bar from "./typescript.ts"`,
           parser,
@@ -68,7 +68,19 @@ context('TypeScript', function () {
           options: ['inline'],
         }),
         test({
+          code: `import { type MyType as Persona } from "./typescript.ts"`,
+          parser,
+          settings,
+          options: ['inline'],
+        }),
+        test({
           code: `import { type MyType, Bar, MyEnum } from "./typescript.ts"`,
+          parser,
+          settings,
+          options: ['inline'],
+        }),
+        test({
+          code: `import { type MyType as Persona, Bar as Bar, MyEnum } from "./typescript.ts"`,
           parser,
           settings,
           options: ['inline'],
@@ -86,6 +98,16 @@ context('TypeScript', function () {
           output: 'import {Bar} from "./typescript.ts"\nimport type { MyType } from "./typescript.ts"',
         }),
         test({
+          code: 'import {type MyType as Persona,Bar} from "./typescript.ts"',
+          parser,
+          settings,
+          options: ['separate'],
+          errors: [{
+            message: 'BOOM',
+          }],
+          output: 'import {Bar} from "./typescript.ts"\nimport type { MyType as Persona } from "./typescript.ts"',
+        }),
+        test({
           code: 'import {type MyType,Bar,type Foo} from "./typescript.ts"',
           parser,
           settings,
@@ -94,6 +116,16 @@ context('TypeScript', function () {
             message: 'BOOM',
           }],
           output: 'import {Bar} from "./typescript.ts"\nimport type { MyType, Foo } from "./typescript.ts"',
+        }),
+        test({
+          code: 'import {type MyType as Persona,Bar,type Foo as Baz} from "./typescript.ts"',
+          parser,
+          settings,
+          options: ['separate'],
+          errors: [{
+            message: 'BOOM',
+          }],
+          output: 'import {Bar} from "./typescript.ts"\nimport type { MyType as Persona, Foo as Baz } from "./typescript.ts"',
         }),
         test({
           code: 'import {type MyType,Bar as Namespace} from "./typescript.ts"',
@@ -125,7 +157,7 @@ context('TypeScript', function () {
           }],
           output: 'import type { MyType as Bar, Foo} from "./typescript.ts"',
         }),
-        // the space is left over when 'type' is removed. Question
+        // the space is left over when 'type' is removed. Question. TODO
         test({
           code: 'import type {MyType} from "./typescript.ts"',
           parser,
@@ -157,6 +189,16 @@ context('TypeScript', function () {
           output: 'import {MyEnum, type MyType, type Bar} from "./typescript.ts"',
         }),
         test({
+          code: 'import type {MyType as Persona, Bar as Foo} from "./typescript.ts";import {MyEnum} from "./typescript.ts"',
+          parser,
+          settings,
+          options: ['inline'],
+          errors: [{
+            message: 'BOOM',
+          }],
+          output: 'import {MyEnum, type MyType as Persona, type Bar as Foo} from "./typescript.ts"',
+        }),
+        test({
           code: 'import type {MyType, Bar} from "./typescript.ts";import {default as B} from "./typescript.ts"',
           parser,
           settings,
@@ -175,6 +217,16 @@ context('TypeScript', function () {
             message: 'BOOM',
           }],
           output: 'import defaultExport, { type MyType, type Bar } from "./typescript.ts"',
+        }),
+        test({
+          code: 'import type {MyType as Persona, Bar as Foo} from "./typescript.ts";import defaultExport from "./typescript.ts"',
+          parser,
+          settings,
+          options: ['inline'],
+          errors: [{
+            message: 'BOOM',
+          }],
+          output: 'import defaultExport, { type MyType as Persona, type Bar as Foo } from "./typescript.ts"',
         }),
         // // TODO: what to do here? Output: import * as b, { type MyType, type Bar } from "./typescript.ts" ?
         // test({
