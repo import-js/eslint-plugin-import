622a1aa3f63148effbf8cd40f43a05b519b22b24
Ivan Rubinson <soryy708@gmail.com>
2024-03-27 19:31:44 +0200

    extract file

--- src/exportMap/builder.js
+++ src/exportMap/builder.js
@@ -20,6 +20,7 @@ import { availableDocStyleParsers, captureDoc } from './doc';
 import { childContext } from './childContext';
 import { isEsModuleInterop } from './typescript';
 import { Namespace } from './namespace';
+import { processSpecifier } from './specifier';
 
 const log = debug('eslint-plugin-import:ExportMap');
 
@@ -89,39 +90,6 @@ export function recursivePatternCapture(pattern, callback) {
 function thunkFor(p, context) {
   // eslint-disable-next-line no-use-before-define
   return () => ExportMapBuilder.for(childContext(p, context));
-}
-
-function processSpecifier(specifier, astNode, exportMap, namespace) {
-  const nsource = astNode.source && astNode.source.value;
-  const exportMeta = {};
-  let local;
-
-  switch (specifier.type) {
-    case 'ExportDefaultSpecifier':
-      if (!nsource) { return; }
-      local = 'default';
-      break;
-    case 'ExportNamespaceSpecifier':
-      exportMap.namespace.set(specifier.exported.name, Object.defineProperty(exportMeta, 'namespace', {
-        get() { return namespace.resolveImport(nsource); },
-      }));
-      return;
-    case 'ExportAllDeclaration':
-      exportMap.namespace.set(specifier.exported.name || specifier.exported.value, namespace.add(exportMeta, specifier.source.value));
-      return;
-    case 'ExportSpecifier':
-      if (!astNode.source) {
-        exportMap.namespace.set(specifier.exported.name || specifier.exported.value, namespace.add(exportMeta, specifier.local));
-        return;
-      }
-    // else falls through
-    default:
-      local = specifier.local.name;
-      break;
-  }
-
-  // todo: JSDoc
-  exportMap.reexports.set(specifier.exported.name, { local, getImport: () => namespace.resolveImport(nsource) });
 }
 
 export default class ExportMapBuilder {
