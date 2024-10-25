2afd06df6b059c1f251cf4cc97f3a5747b31f6f4
Michaël De Boey <info@michaeldeboey.be>
2024-10-25 04:53:22 +0200

    feat: drop Node 4, 6, 8, 10, 12, 14 & 16 support
    BREAKING CHANGE: Requires Node@^18.18.0 || ^20.9.0 || >=21.1.0

--- tests/dep-time-travel.sh
+++ tests/dep-time-travel.sh
@@ -2,7 +2,7 @@
 
 # expected: ESLINT_VERSION numeric env var
 
-echo "installing ${ESLINT_VERSION} in node ${TRAVIS_NODE_VERSION} with TS parser ${TS_PARSER:-default}..."
+echo "installing ${ESLINT_VERSION} with TS parser ${TS_PARSER:-default}..."
 
 export NPM_CONFIG_LEGACY_PEER_DEPS=true
 
@@ -19,10 +19,6 @@ if [[ -n "$TS_PARSER" ]]; then # if TS parser is manually set, always use it
 elif [[ "$ESLINT_VERSION" -lt "5" ]]; then # completely remove the new TypeScript parser for ESLint < v5
   echo "Removing @typescript-eslint/parser..."
   npm uninstall --no-save @typescript-eslint/parser
-elif [[ "$TRAVIS_NODE_VERSION" -lt "10" ]]; then # TS parser 3 requires node 10+
-  npm i --no-save "@typescript-eslint/parser@3"
-elif [[ "$TRAVIS_NODE_VERSION" -lt "12" ]]; then # TS parser 4 requires node 12+
-  npm i --no-save "@typescript-eslint/parser@4"
 fi
 
 # use these alternate TypeScript dependencies for ESLint < v4
@@ -35,12 +31,6 @@ if [[ "$ESLINT_VERSION" -lt "4" ]]; then
 elif [[ "$ESLINT_VERSION" -lt "7" ]]; then
   echo "Downgrading TypeScript dependencies..."
   npm i --no-save typescript-eslint-parser@20
-fi
-
-# typescript-eslint-parser 1.1.1+ is not compatible with node 6
-if [[ "$TRAVIS_NODE_VERSION" -lt "8" ]]; then
-  echo "Downgrading eslint-import-resolver-typescript..."
-  npm i --no-save eslint-import-resolver-typescript@1.0.2
 fi
 
 if [ "${ESLINT_VERSION}" = '8' ]; then
